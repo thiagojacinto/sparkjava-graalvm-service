5f153b0635cdcbda4bb28528f3774d3b4ac3aa5d
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2024-04-23 22:53:06 -0300

    fix: adicionando validacoes antes de executar os testes

--- executar-teste-local.sh
+++ executar-teste-local.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
+set -e
 
 # Use este script para executar testes locais
 
 RESULTS_WORKSPACE="$(pwd)/load-test/user-files/results"
 GATLING_BIN_DIR=$GATLING_HOME/bin
 GATLING_WORKSPACE="$(pwd)/load-test/user-files"
+
+if [ -z ${GATLING_HOME} ]; then
+    echo '[error]   A variavel $GATLING_HOME deve apontar para o diretório do Gatling.';
+    exit 1;
+fi
+
+if [ -f ${GATLING_BIN_DIR} ]; then
+    echo '[error]   A variavel $GATLING_BIN_DIR deve apontar para o binário do Gatling.';
+    exit 2;
+fi
 
 runGatling() {
     sh $GATLING_BIN_DIR/gatling.sh -rm local -s RinhaBackendCrebitosSimulation \
